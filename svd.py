Starting the eigen vector sign ambiguity fix

svd.py
@@ -16,31 +16,34 @@ class SVD():
     #Training and Valdiation dataset
     data_matrix=None
     validation_matrix=None
+    reconstructed_matrix=None
     #SVD attributes CAUTION:(they are in ascending order)
     sigma_vector=None
     sigma2_vector=None
     U_matrix=None
     V_matrix=None
     svd_filename='svd_decomposition.npz'
-    #Additional attributes for prediction phase
+    #Additional attributes for reconstruction phase
     user_mean_vec=None      #Will keep the mean rating of user
     user_var_vec=None       #will kepp the variance in the user rating
 
     ################# Member Functions #################
-    def __init__(self,filepath):
+    def __init__(self,filepath,mode):
         '''
         Here we will initialize the svd class by loading the data
         and initializing other variables.
         USAGE:
             INPUT:
                 filepath    : the path where the datamatrix is stored
+                mode        : load/normalize, normalize the data/rating
+                                matrix. if load just load the data matrix
         '''
         #Loading the data matrix into the memory
         print ("Loading the data matrix")
         self.data_matrix,self.validation_matrix=load_rating_matrix(filepath)
         #Normalizing the dataset before use
         print ("Normalizing the dataset")
-        self.normalize_dataset()
+        self.normalize_dataset(mode)
 
         #Loading the svd decomposition into memory if present
         try:
@@ -56,7 +59,7 @@ class SVD():
             print ("Saving the SVD Decomposition in dataset directory")
             self._save_svd_decomposition(filepath)
 
-    def normalize_dataset(self):
+    def normalize_dataset(self,mode):
         '''
         (THINK/TUNABLE)
         This function will normalize the dataset to make the non rated
@@ -65,6 +68,7 @@ class SVD():
         the rating vector of each user to bring their rating to same scale
         '''
         #Casting the dataset to floating point precision from uint8
+        self.data_matrix=np.array([[3,2,2],[2,3,-2]])
         self.data_matrix=(self.data_matrix).astype(np.float64)
 
         #Now calculationg the mean and varaince of each user rating
@@ -72,12 +76,14 @@ class SVD():
         self.user_var_vec=np.var(self.data_matrix,axis=1,keepdims=True)
 
         #Now normalizing the data/rating-matrix
-        #Subtracting the mean
-        mask=(self.data_matrix!=0)
-        masked_mean=mask*self.user_mean_vec #element wise broadcasted along user
-        self.data_matrix=self.data_matrix-masked_mean
-        #Diving with the varaince to brong every rating to same scale
-        self.data_matrix=self.data_matrix/self.user_var_vec #element-broad user
+        if mode=='normalize':
+            print 'normalizing the data matrix'
+            #Subtracting the mean
+            mask=(self.data_matrix!=0)
+            masked_mean=mask*self.user_mean_vec #element wise broadcasted along user
+            self.data_matrix=self.data_matrix-masked_mean
+            #Diving with the varaince to brong every rating to same scale
+            self.data_matrix=self.data_matrix/self.user_var_vec #element-broad user
 
     def generate_svd(self):
         '''
@@ -111,6 +117,11 @@ class SVD():
 
         #Removing the zero eigen values since both aat and ata
         #will have same eigen value (larger oneses has extra zeros)
+        print evalU
+        print evalV
+        print evecU
+        print evecV
+        print self.data_matrix
         size=None
         eval=None
         if(evalU.shape[0]>evalV.shape[0]):
@@ -123,13 +134,36 @@ class SVD():
             size=evalU.shape[0]
             eval=evalU
             #Removing the trivial (zero) eigen vectors) from V
-            self.V_matrix=self.V_matrix[:,-size,:]
+            self.V_matrix=self.V_matrix[:,-size:]
 
         #Getting the sigma matrix from the eigen value
         print ("Creating the Sigma Diagonal Matrix")
         self.sigma2_vector=eval
         sigma=np.sqrt(np.abs(eval))
         self.sigma_vector=sigma #np.diag(sigma)
+
+    def create_reconstruction(self,mode=None):
+        '''
+        This function will reconstruct the rating matrix, using the
+        decomposed component and also denormalize it to have
+        same mean and variance for each user.
+        (matching their rating style)
+        USAGE:
+            INPUT:
+                mode    : where we have to denormalize the reconstructed
+                            matrix usign the mean rating of the user
+        '''
+        print "Reconstructing the rating-matrix"
+        #Reconstructing the rating matrix
+        recon=np.dot(self.U_matrix,np.diag(self.sigma_vector))
+        recon=np.dot(recon,self.V_matrix.T)
+        #self.reconstructed_matrix=recon
+
+        #Since this will be a normalized matrix denormalize it
+        if mode=='normalize':
+            print "Renormalizing the rating-matrix"
+            recon=recon*self.user_var_vec+self.user_mean_vec
+        self.reconstructed_matrix=recon
 
     def _get_eigen_vectors(self,K):
         '''
@@ -161,7 +195,8 @@ class SVD():
                             U_matrix=self.U_matrix,
                             V_matrix=self.V_matrix,
                             sigma_vector=self.sigma_vector,
-                            sigma2_vector=self.sigma2_vector)
+                            sigma2_vector=self.sigma2_vector,)
+                            #reconstructed_matrix=self.reconstructed_matrix)
         # #No need to store the data matrix now
         # self.data_matrix=None
 
@@ -182,8 +217,9 @@ class SVD():
         self.sigma2_vector=load_dict['sigma2_vector']
         self.U_matrix=load_dict['U_matrix']
         self.V_matrix=load_dict['V_matrix']
+        # self.reconstructed_matrix=load_dict['reconstructed_matrix']
 
-    def _set_90percent_energy_mode(self):
+    def _set_90percent_energy_mode(self,keep_energy=0.90):
         '''
         This function will update the eigen vectors and the eigen
         value matrices to reatin the energy only upto 90%.
@@ -202,7 +238,7 @@ class SVD():
             energy_left=total_energy-energy[i]
 
             #Increamenting the leaving pointer
-            if (energy_left/total_energy>=0.98):
+            if (energy_left/total_energy>=keep_energy):
                 start_index+=1
             else:
                 break
@@ -210,8 +246,8 @@ class SVD():
         #Now removing the insignificant energy and corresponding eigen vectors
         self.sigma_vector=self.sigma_vector[start_index:,]
         self.sigma2_vector=self.sigma2_vector[start_index:,]
-        self.U_matrix=self.U_matrix[start_index:,:]
-        self.V_matrix=self.V_matrix[start_index:,:]
+        self.U_matrix=self.U_matrix[:,start_index:]
+        self.V_matrix=self.V_matrix[:,start_index:]
 
         print ("90 percent Energy mode on")
 
@@ -246,19 +282,86 @@ class SVD():
 
         return similarity
 
+    def get_rmse_reconstruction_error(self):
+        '''
+        This function will calculate the root-mean squared error
+        of the reconstruction and the actual matrix.
+        USAGE:
+            OUTPUT:
+                rmse_val    : the average root mean squared error.
+        '''
+        diff=self.data_matrix-self.reconstructed_matrix
+        rmse_val=np.mean(np.square(diff))**(0.5)
+
+        #Printing the real utility matrix and prediction
+        for i in range(self.data_matrix.shape[0]):
+            for j in range(self.data_matrix.shape[1]):
+                if (self.data_matrix[i,j]==0):
+                    continue
+                print ('actual:{}   prediction:{}    diff:{}'.format(
+                        self.data_matrix[i,j],
+                        self.reconstructed_matrix[i,j],
+                        diff[i,j]))
+
+        return rmse_val
+
+    def get_validation_error(self):
+        '''
+        This function will calculate the validation set error,
+        by comparing the prediction made by the reconstructed matrix
+        and the actal value
+        USAGE:
+            OUTPUT:
+                rmse_val    : the root mean squared error value.
+        '''
+        rmse_val=0
+
+        #Iterating over the validation examples
+        N=self.validation_matrix.shape[0]
+        for i in range(N):
+            #Taking out the user and movie id
+            user_id=self.validation_matrix[i,0]-1
+            movie_id=self.validation_matrix[i,1]-1
+
+            rating_diff =   self.validation_matrix[i,2]-\
+                        self.reconstructed_matrix[user_id,movie_id],
+            rating_diff=np.squeeze(rating_diff)
+
+            #Printing for interactiveness
+            print ("actual:{} prediction:{} diff:{}".format(
+                self.validation_matrix[i,2],#actual rating
+                self.reconstructed_matrix[user_id,movie_id],
+                rating_diff,
+            ))
+
+            #Storing the squared error
+            rmse_val+=(rating_diff**2)
+
+        #Taking the mean value of squared error
+        rmse_val=(rmse_val/N)**(0.5)
+
+        return rmse_val
+
 if __name__=='__main__':
+    #Creating the svd object
     filepath='ml-1m/'
-    svd=SVD(filepath)
+    mode='no_normalize'
+    svd=SVD(filepath,mode)
+
+    #Checking the size compatibility
+    print svd.data_matrix.shape
     print svd.U_matrix.shape
     print svd.V_matrix.shape
     print svd.sigma_vector.shape
-    # sigma_vector=svd.sigma_vector
-    # print sigma_vector.shape
-    # for i in range(sigma_vector.shape[0]):
-    #     print sigma_vector[i]
-    # svd._set_90percent_energy_mode()
-    # sigma_vector=svd.sigma_vector
-    # print sigma_vector.shape
-    # for i in range(sigma_vector.shape[0]):
-    #     print sigma_vector[i]
-    # svd.make_prediction(1,1193)
+
+    #Recosntructing the svd decomposition
+    svd.create_reconstruction()
+    # print 'vaid:',svd.get_validation_error()
+    print 'recon:',svd.get_rmse_reconstruction_error()
+
+
+    #Reconstruction with 90% eneergy left
+    # svd._set_90percent_energy_mode(keep_energy=0.90)
+    # svd.create_reconstruction()
+    # print 'vaid:',svd.get_validation_error()
+    # print 'recon:',svd.get_rmse_reconstruction_error()